084415464d58a3a73aa9539e346151a21b00f97a
baptiste lhopitault <baptistelhopitault@blablablablabla.home>
2014-12-27 15:15:56 +0100

    Finitions page descriptions

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html,body').animate({
-            scrollTop: (target.offset().top)-90
+            scrollTop: (target.offset().top)-80
           }, 1000);
           return false;
         }
