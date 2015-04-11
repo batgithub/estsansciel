d5d2ce3c53cf48fdfee8fd8b72725a9938af4dd2
baptiste lhopitault <baptiste@lehackintosh.bouyguesbox.fr>
2015-04-11 12:06:58 +0200

    correction padding slider + test underline

--- js/app.js
+++ js/app.js
@@ -1,22 +1,39 @@
 $(document).ready(function(){
     $('.creations-carousel').slick({
         dots:true,
-        infinite:false
+        infinite:false        
     });
 });
 
 
+jQuery("#play").on('click',function(){
+    var vi = jQuery("#video");
+    vi.attr("src", vi.data("autoplay-src") );
+});
+
+$(window).bind('load scroll', underline($('#who')));
 
 
-$('#play').on('click', function(ev) {
-    $('#video').removeClass("hidden");
-    $('#play').addClass("hidden");
-    $('#image').addClass("hidden");
-    $("#video")[0].src += "&autoplay=1";
-    ev.preventDefault();
- 
-  });
+function underline(element)
+{
+    //Calcule de la position du bas de la fenètre, en hauteur dans le site
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+   
+    
+    //Calcule de la position de notre élément
+    var elemTop = element.offset().top;
+    var elemBottom = elemTop + element.height(); 
 
+
+    //si la fenètre dépasse le bas de la div 
+    if ((elemBottom <= docViewBottom) && (elemTop >= docViewTop))
+    {
+      $(window).on('load scroll');
+      element.addClass("underline");
+    }
+   
+}
 
 
 
